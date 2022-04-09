a6a591b1203ace6295787eb7b3caec279e76db39
xiao_meng <2289895723@qq.com>
2022-04-10 01:09:41 +0800

    V1.0.0.0410

---
+++ SoKoBan V1.0.0.0410/sokobanmap.cpp
@@ -0,0 +1,18 @@
+#include "sokobanmap.h"
+#include<iostream>
+
+using namespace std;
+
+void ShowText(long long rmm_size, long long cache_size)
+{
+	cout << "输入主存大小___?___MB" << endl;
+	cin >> rmm_size;
+	cout << "输入Cache大小___?___KB" << endl;
+	cin >> cache_size;
+	cout << rmm_size << " " << cache_size << endl;
+
+}
+
+
+
+
---
+++ SoKoBan V1.0.0.0410/sokobanmap.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef SOKOBANMAP_H
+#define SOKOBANMAP_H
+
+void ShowText(long long rmm_size, long long cache_size);
+
+#endif
